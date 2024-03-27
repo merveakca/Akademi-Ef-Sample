2b0110de6c347e42cb4d075e5845348b7f1f2620
merveakca <smkirac@gmail.com>
2024-03-27 04:15:37 +0300

    Add project files.

---
+++ EfSample/Models/ORM/AdminUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EfSample.Models.ORM;
+public class AdminUser
+{
+    public int Id { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+}
---
+++ EfSample/Models/ORM/AkademiLibraryContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EfSample.Models.ORM;
+public class AkademiLibraryContext : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=DESKTOP-D84DF12\\SQLEXPRESS02; Database=AkademiDb; trusted_connection=true; TrustServerCertificate=true");
+    }
+
+    public DbSet<Book> Books { get; set; }
+    public DbSet<AdminUser> AdminUsers { get; set; }
+}
